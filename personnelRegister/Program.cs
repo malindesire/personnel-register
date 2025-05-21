504a9e3fd79b20a40d6e87e53d6a66668924d214
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-05-21 16:40:38 +0200

    Add menu and separate to methods

--- personnelRegister/Program.cs
+++ personnelRegister/Program.cs
@@ -6,6 +6,39 @@
         {
             List<Employee> employees = new List<Employee>();
 
+            showMenu();
+            string selectedAction = Console.ReadLine();
+
+            while (selectedAction != "99")
+            {
+                switch (selectedAction)
+                {
+                    case "1":
+                        addEmployee(employees);
+                        break;
+                    case "2":
+                        showRegister(employees);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+
+                showMenu();
+                selectedAction = Console.ReadLine();
+            }
+        }
+
+        private static void showMenu()
+        {
+            Console.WriteLine("Choose the action you want to do:");
+            Console.WriteLine("1. Add new employee");
+            Console.WriteLine("2. Show employee register");
+            Console.WriteLine("99. Exit");
+        }
+
+        private static List<Employee> addEmployee(List<Employee> employees)
+        {
             Console.WriteLine("Ange namn:");
             string name = Console.ReadLine();
             Console.WriteLine("Ange lön:");
@@ -13,10 +46,13 @@
 
             employees.Add(new Employee(name, salary));
 
+            return employees;
+        }
+        private static void showRegister(List<Employee> employees)
+        {
             foreach (Employee employee in employees)
             {
-                Console.WriteLine("Full name: " + employee.fullName);
-                Console.WriteLine("Monthly salary: " + employee.monthlySalary);
+                Console.WriteLine("Full name: " + employee.fullName + ", Monthly salary: " + employee.monthlySalary);
             }
         }
     }
