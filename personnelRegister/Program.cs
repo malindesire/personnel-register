09b0d60ac8c61cb5d34b682a10fc957c990c1886
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-05-21 15:55:39 +0200

    Add basic functionality

--- personnelRegister/Program.cs
+++ personnelRegister/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace personnelRegister;
-
-class Program
+﻿namespace personnelRegister
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello world!");
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Console.WriteLine("Ange namn:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ange lön:");
+            int salary = int.Parse(Console.ReadLine());
+
+            employees.Add(new Employee(name, salary));
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine("Full name: " + employee.fullName);
+                Console.WriteLine("Monthly salary: " + employee.monthlySalary);
+            }
+        }
+    }
+
+    public class Employee
+    {
+        public string fullName;
+        public int monthlySalary;
+
+        public Employee(string name, int salary)
+        {
+            fullName = name;
+            monthlySalary = salary;
+        }
     }
 }
+
